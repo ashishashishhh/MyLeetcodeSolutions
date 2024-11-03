053d964260a91b11de397e0c30f777c61fcfd097
Ashish Alok <72861209+ashishashishhh@users.noreply.github.com>
2024-11-03 15:53:25 +0530

    Time: 34 ms (30.09%), Space: 27.4 MB (24.64%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,29 +2,27 @@ class Solution {
 public:
     int fun(vector<int>& coins, int amount, int i, vector<vector<int>>& memo) {
         // Base cases
-        if (amount == 0) return 1; // If no amount is left, no coins are needed
+        if (amount == 0)
+         return 1; 
         if (amount < 0 || i >= coins.size())
-         return 0; // Return large number if amount is negative or no coins left
+         return 0; 
         
-        // Check if the result is already computed
-        if (memo[i][amount] != -1) return memo[i][amount];
+     
+        if (memo[i][amount] != -1)
+         return memo[i][amount];
         
-        // Option 1: Do not take the current coin
         int nottake = fun(coins, amount, i + 1, memo);
         
-        // Option 2: Take the current coin if it can fit
         int take = 0;
         if (coins[i] <= amount) {
-            take = fun(coins, amount - coins[i], i, memo); // Include this coin and continue with the same index
+            take = fun(coins, amount - coins[i], i, memo); 
         }
         
-        // Store the result in the memo table
         memo[i][amount] = take + nottake;
         return memo[i][amount];
     }
     int change(int amount, vector<int>& coins) {
         vector<vector<int>> memo(coins.size(), vector<int>(amount + 1, -1));
         return fun(coins, amount, 0, memo);
-        
     }
 };
