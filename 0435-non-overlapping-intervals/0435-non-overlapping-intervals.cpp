0885bab27c1d5ac01bd6cd4a93b4766c992a824d
Ashish Alok <72861209+ashishashishhh@users.noreply.github.com>
2024-11-05 16:36:30 +0530

    Time: 57 ms (48.29%), Space: 93.5 MB (92.97%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    #include <vector>
-#include <algorithm>
-
-int eraseOverlapIntervals(std::vector<std::vector<int>>& intervals) {
-    if (intervals.empty()) return 0;
-
-    // Sort intervals by their end time
-    std::sort(intervals.begin(), intervals.end(), [](const std::vector<int>& a, const std::vector<int>& b) {
-        return a[1] < b[1];
-    });
-
-    int removeCount = 0;
-    int end = intervals[0][1]; // Initialize end time with the end of the first interval
-
-    // Traverse through intervals
-    for (int i = 1; i < intervals.size(); ++i) {
-        if (intervals[i][0] < end) {
-            // Overlap, so we need to remove this interval
-            removeCount++;
-        } else {
-            // No overlap, update end to the end of the current interval
-            end = intervals[i][1];
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[1] < b[1];
+        });
+        int i=0,j=1,ans=0;
+        while(i<intervals.size() && j<intervals.size())
+        {
+            int end = intervals[i][1];
+            int start = intervals[j][0];
+            if(end>start){
+                ans++;
+                j++;
+            }
+            else{
+                i=j;
+                j++;
+            }
         }
+        return ans;
+        
     }
-
-    return removeCount;
-}
-
 };
