82e2f05bc2c9916fe3e259d1aa6c72129abc2f33
Ashish Alok <72861209+ashishashishhh@users.noreply.github.com>
2024-11-08 13:37:51 +0530

    Time: 0 ms (100%), Space: 12.8 MB (27.06%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -1,24 +1,31 @@
 class Solution { 
 public:
 vector<int> dp;
-    int sub(vector<int> &day, vector<int> &cost, int si)    // subproblem
+    int sub(vector<int> &day, vector<int> &cost, int si)   
     {
         int n = day.size();
-        if(si>=n)   return 0;
+        if(si>=n)
+           return 0;
         
-        if(dp[si])  return dp[si];      // ie we aready knows the answer so no need to re calculate , return answer.
+        if(dp[si])
+          return dp[si]; 
         
         int cost_day = cost[0] + sub(day , cost , si+1);
         
-        int i;
-        for(i = si ; i<n and day[i]<day[si]+7 ; i++);
+        int i = si;
+
+        while(i<n && day[i]<day[si]+7)
+          i++;
+
         int cost_week = cost[1] + sub(day, cost, i);
         
-        for(i = si ; i<n and day[i]<day[si]+30 ; i++);
+        while(i<n && day[i]<day[si]+30)
+          i++;
+          
         int cost_month = cost[2] + sub(day, cost, i);     
         
-        dp[si] = min({cost_day, cost_week , cost_month  });   // we store answer for future references 
-        
+        dp[si] = min({cost_day, cost_week , cost_month  });
+
         return dp[si];
     } 
     int mincostTickets(vector<int>& days, vector<int>& costs) {        
