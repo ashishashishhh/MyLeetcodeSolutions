641fd25cec56af1a52af478802fb583b2e1f8a01
Ashish Alok <72861209+ashishashishhh@users.noreply.github.com>
2024-11-11 12:44:10 +0530

    Time: 102 ms (20.8%), Space: 135.8 MB (19.68%) - LeetHub

---
+++ 0819-minimum-swaps-to-make-sequences-increasing/0819-minimum-swaps-to-make-sequences-increasing.cpp
@@ -0,0 +1,44 @@
+class Solution {
+private:
+        int solveMem(vector<int>& nums1, vector<int>& nums2,int index,int swapped,vector<vector<int>> &dp)
+    {
+         //base case
+         if(index==nums1.size()) return 0;
+
+         if(dp[index][swapped]!=-1) return dp[index][swapped];
+
+         int ans=INT_MAX;
+
+         int prev1=nums1[index-1];
+         int prev2=nums2[index-1];
+
+         //catch
+         if(swapped) swap(prev1,prev2);
+
+         //noswap
+         if(nums1[index]>prev1 && nums2[index]>prev2) ans=solveMem(nums1,nums2,index+1,0,dp);
+         
+         //swap
+         if(nums1[index]>prev2 && nums2[index]>prev1)
+         {
+             ans=min(ans,1+solveMem(nums1,nums2,index+1,1,dp));
+         }
+
+         return dp[index][swapped]=ans;
+    }
+
+
+public:
+    int minSwap(vector<int>& nums1, vector<int>& nums2) {
+
+         nums1.insert(nums1.begin(),-1);
+         nums2.insert(nums2.begin(),-1);
+        //it means that the previous indexes were swapped or not 
+         bool swapped=0;
+
+         int n=nums1.size();
+
+         vector<vector<int>> dp(n,vector<int> (2,-1));
+         return solveMem(nums1,nums2,1,swapped,dp);
+    }
+};
