0af306da82f221abc9be640d80c228c08998702d
Ashish Alok <72861209+ashishashishhh@users.noreply.github.com>
2024-12-23 17:45:47 +0530

    Time: 489 ms (13.64%), Space: 148 MB (100%) - LeetHub

--- 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
+++ 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int maxDistinctElements(vector<int>& nums, int k) {
-    sort(nums.begin(), nums.end()); // Sort the input array
-    int lastUsed = INT_MIN; // Track the largest adjustment used so far
-    int count = 0; // Count of unique adjustments
+        sort(nums.begin(), nums.end()); 
+        unordered_set<int>st;
+        int last = INT_MIN;
+        for(int num:nums){
+           int curr = max(last + 1 , num - k);
+           cout<<curr<<endl;
+           if(curr<=num + k){
+            st.insert(curr);
+            last = curr;
+           }
 
-    for (int num : nums) {
-        // Choose the maximum of num - k or lastUsed + 1 as the next valid adjustment
-        int adjustment = max(num - k, lastUsed + 1);
-        if (adjustment <= num + k) {
-            lastUsed = adjustment; // Update lastUsed to the chosen adjustment
-            ++count; // Increment the count of unique adjustments
         }
-    }
-
-    return count; // Return the count of unique adjustments
+        return st.size();
     }
 };
