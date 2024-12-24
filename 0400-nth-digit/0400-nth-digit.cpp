b1f7caf9b31b1d9a0b081a24a3df28a4fa71638c
Ashish Alok <72861209+ashishashishhh@users.noreply.github.com>
2024-12-24 15:56:49 +0530

    Time: 2 ms (100%), Space: 8 MB (11.05%) - LeetHub

---
+++ 0400-nth-digit/0400-nth-digit.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findNthDigit(int n) {
+        if (n <= 9)
+            return n;
+        
+        long long base = 9;
+        long long digits = 1;
+
+        while ((long long)n > (base * digits)) {
+            n -= (base * digits);
+            base *= 10;
+            digits += 1;
+        }
+        cout<<n<<" "<<base<<" "<<digits<<endl;
+        long long num = pow(10, (digits - 1)) + (n - 1) / digits;
+        cout<<num;
+        long long index = (n - 1) % digits;
+
+        return to_string(num)[index] - '0';
+    }
+};
