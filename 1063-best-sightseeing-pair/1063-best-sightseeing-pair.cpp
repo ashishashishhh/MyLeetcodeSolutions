1b4c169fc92b82fe33eae75b8fc55fa732fddf15
Ashish Alok <72861209+ashishashishhh@users.noreply.github.com>
2024-12-27 23:01:54 +0530

    Time: 0 ms (100%), Space: 45.3 MB (69.53%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
-        
-        int diff = INT_MIN;
-        int value = values[0];
-        int index = 0;
+        int diff = INT_MIN ,value = values[0] , index = 0;
         for(int i=1;i<values.size();i++){
             diff = max(diff , values[i] + value + index - i);
             if((values[i] + i) >= (value + index)){
@@ -12,7 +9,6 @@ public:
                 index = i;
             }
         }
-
         return diff;
     }
 };
