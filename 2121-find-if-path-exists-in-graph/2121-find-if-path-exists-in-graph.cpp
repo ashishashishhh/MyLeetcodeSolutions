51f80df4394db8d59a57d227a8e0c994f98a58b7
Ashish Alok <72861209+ashishashishhh@users.noreply.github.com>
2025-01-24 17:04:10 +0530

    Time: 321 ms (63.08%), Space: 302.3 MB (63.84%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    bool check(int source, vector<vector<int>>& graph, vector<int>& visited, int dest) {
-        if (source == dest) return true; // Base case: if source equals destination
-        visited[source] = 1;            // Mark the current node as visited
-        for (auto neighbor : graph[source]) {
-            if (!visited[neighbor] && check(neighbor, graph, visited, dest))
-                return true;
+    bool check(int source , vector<vector<int>>&graph,vector<int>&boo,int dest){
+        if(source == dest)
+        return true;
+        boo[source] = 1;
+        for(auto x:graph[source]){
+            if(!boo[x] && check(x,graph,boo,dest))
+            return true;
         }
-        return false; // Path not found
+        return false;
     }
-    
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        vector<vector<int>> graph(n);
-        for (auto& edge : edges) { 
-            // Constructing an undirected graph
-            graph[edge[0]].push_back(edge[1]);
-            graph[edge[1]].push_back(edge[0]);
+        vector<vector<int>>graph(n);
+        for(int i=0;i<edges.size();i++){
+            int start = edges[i][0];
+            int end = edges[i][1];
+            graph[start].push_back(end);
+            graph[end].push_back(start);
         }
-        
-        vector<int> visited(n, 0); // Visited array to track visited nodes
-        return check(source, graph, visited, destination);
+        vector<int>boo(n,0);
+        return check(source,graph,boo,destination);
     }
 };
